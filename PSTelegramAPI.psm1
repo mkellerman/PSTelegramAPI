8dc22f515c7d373fab5a0fc26fb4729da478a77a
Marc R Kellerman <9411143+mkellerman@users.noreply.github.com>
2019-01-03 13:33:26 -0800

    Update PSTelegramAPI.psm1

--- PSTelegramAPI.psm1
+++ PSTelegramAPI.psm1
@@ -1,7 +1,25 @@
-# Implement your module commands in this script.
+if ($PSEdition -eq "Core") {
+    #Import-Module -Name "$PSScriptRoot/lib/jwt/netstandard1.3/JWT.dll" | Out-Null
+    #Import-Module -Name "$PSScriptRoot/lib/Newtonsoft/netstandard1.0/Newtonsoft.Json.dll" | Out-Null
+}
+else {
+    Import-Module -Name "$PSScriptRoot/lib/tlsharp/TLSharp.Core.dll" | Out-Null
+    #Import-Module -Name "$PSScriptRoot/lib/Newtonsoft/net45/Newtonsoft.Json.dll" | Out-Null
+}
+
+#Get public and private function definition files.
+$Public = @( Get-ChildItem -Path $PSScriptRoot\public\*.ps1 -ErrorAction SilentlyContinue )
+$Private = @( Get-ChildItem -Path $PSScriptRoot\private\*.ps1 -ErrorAction SilentlyContinue )
+
+#Dot source the files
+Foreach ($import in @($Public + $Private)) {
+    Try {
+        . $import.fullname
+    }
+    Catch {
+        Write-Error -Message "Failed to import function $($import.fullname): $_"
+    }
+}
 
 
-# Export only the functions using PowerShell standard verb-noun naming.
-# Be sure to list each exported functions in the FunctionsToExport field of the module manifest file.
-# This improves performance of command discovery in PowerShell.
-Export-ModuleMember -Function *-*
+Export-ModuleMember -Function $Public.Basename
