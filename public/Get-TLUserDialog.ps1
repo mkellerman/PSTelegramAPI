1594d0460d7415c62f8579a79281d96a7810cbb9
Marc R Kellerman <9411143+mkellerman@users.noreply.github.com>
2019-01-03 17:07:30 -0800

    Update Get-TLUserDialog.ps1

--- public/Get-TLUserDialog.ps1
+++ public/Get-TLUserDialog.ps1
@@ -1,40 +1,29 @@
-function Get-TLUserDialog ($TLClient) {
+function Get-TLUserDialog {
+
+    [cmdletbinding()]
+    Param(
+        [Parameter(Mandatory = $true)]
+        [TLSharp.Core.TelegramClient]$TLClient
+    )
 
     $Results = @()
     $lastMessageDate = 0
 
     Do {
 
-        Do {
-
-            Write-Verbose "GetUserDialogsAsync(lastMessageDate: $lastMessageDate)"
-            $Async = $TLClient.GetUserDialogsAsync($lastMessageDate) | Wait-TLAsync
-
-            If ($TimeToWait = $Async.Exception.InnerException.TimeToWait.TotalSeconds) { Start-Sleep -Seconds $TimeToWait }
-            If (-Not($TimeToWait) -and ($Async.Exception.InnerException.Message)) {
-                Throw $Async.Exception.InnerException.Message
-            }
-
-        } Until ($Result = $Async.Result)
+        Write-Verbose "GetUserDialogsAsync(lastMessageDate: $lastMessageDate)"
+        $Result = $TLClient.GetUserDialogsAsync($lastMessageDate) | Wait-TLAsync
 
         $lastMessageDate = $Result.Messages.Date[-1]
         $Results += $Result
 
     } Until ($Result.Dialogs.Count -lt 100)
 
-    $Users    = $Results.Users | ? { $_.GetType().Name -match 'TLUser' } | ConvertFrom-TLObject
-    $Chats    = $Results.Chats | ? { $_.GetType().Name -match 'TLChat' } | ConvertFrom-TLObject
-    $Channels = $Results.Chats | ? { $_.GetType().Name -match 'TLChannel' } | ConvertFrom-TLObject
-
-    $Dialogs = $Results.Dialogs | ConvertFrom-TLObject
-    $Messages = $Results.Messages | ConvertFrom-TLObject
-
     $Result = [PSCustomObject]@{
-        User = $Users
-        Chat = $Chats
-        Channel = $Channel
-        Dialog = $Dialogs
-        Message = $Messages
+        User = $Results.Users
+        Chat = $Results.Chats
+        Dialog = $Results.Dialogs
+        Message = $Results.Messages
     }
 
     Return $Result
