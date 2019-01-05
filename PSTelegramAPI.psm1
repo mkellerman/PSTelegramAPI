b93ab751baa61616b07d81a10c744e183ea24ae7
Marc R Kellerman <9411143+mkellerman@users.noreply.github.com>
2019-01-05 09:19:59 -0800

    Update to TLSharp folder sturcture

--- PSTelegramAPI.psm1
+++ PSTelegramAPI.psm1
@@ -1,17 +1,13 @@
-if ($PSEdition -eq "Core") {
-    #Import-Module -Name "$PSScriptRoot/lib/jwt/netstandard1.3/JWT.dll" | Out-Null
-    #Import-Module -Name "$PSScriptRoot/lib/Newtonsoft/netstandard1.0/Newtonsoft.Json.dll" | Out-Null
-}
-else {
-    Import-Module -Name "$PSScriptRoot/lib/tlsharp/TLSharp.Core.dll" | Out-Null
-    #Import-Module -Name "$PSScriptRoot/lib/Newtonsoft/net45/Newtonsoft.Json.dll" | Out-Null
-}
+
+# Import TLSharp binaries.
+$TLSharpDll = Get-ChildItem -Path $PSScriptRoot\lib\ -Filter TLSharp.Core.dll -Recurse
+Import-Module -Name $TLSharpDll.FullName | Out-Null
 
 #Get public and private function definition files.
 $Public = @( Get-ChildItem -Path $PSScriptRoot\public\*.ps1 -ErrorAction SilentlyContinue )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\private\*.ps1 -ErrorAction SilentlyContinue )
 
-#Dot source the files
+#Dot source the files.
 Foreach ($import in @($Public + $Private)) {
     Try {
         . $import.fullname
@@ -20,6 +16,5 @@ Foreach ($import in @($Public + $Private)) {
         Write-Error -Message "Failed to import function $($import.fullname): $_"
     }
 }
-
 
 Export-ModuleMember -Function $Public.Basename
