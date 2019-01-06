255c2f10742e909b977c2e2a03b41aaaba9f87b3
Marc R Kellerman <9411143+mkellerman@users.noreply.github.com>
2019-01-05 22:07:04 -0800

    Merged RequiredModules.psd1

---
+++ PSTelegramAPI.bootstrap.ps1
@@ -0,0 +1,23 @@
+using namespace Microsoft.PowerShell.Commands
+[CmdletBinding()]
+param(
+    #
+    [ValidateSet("CurrentUser", "AllUsers")]
+    $Scope = "CurrentUser"
+)
+
+[ModuleSpecification[]]$RequiredModules = @(
+    @{ ModuleName = "InvokeBuild"; RequiredVersion = "5.4.2"}
+    @{ ModuleName = "Pester"; RequiredVersion = "4.4.4"}
+)
+
+$Policy = (Get-PSRepository PSGallery).InstallationPolicy
+Set-PSRepository PSGallery -InstallationPolicy Trusted
+
+try {
+    $RequiredModules | Install-Module -Scope $Scope -Repository PSGallery -SkipPublisherCheck -Verbose
+} finally {
+    Set-PSRepository PSGallery -InstallationPolicy $Policy
+}
+
+$RequiredModules | Import-Module
--- RequiredModules.psd1
+++
@@ -1,5 +0,0 @@
-@(
-    @{ ModuleName = "InvokeBuild"; RequiredVersion = "5.4.2"}
-    @{ ModuleName = "Pester"; RequiredVersion = "4.4.4"}
-    @{ ModuleName = "PlatyPS"; RequiredVersion = "0.12.0"}
-)
